e051846337fa469d6ecdbaabe62c07c0d3a1d78c
abdulsalamp <pabdulsalam@hotmail.com>
2023-10-23 23:45:23 +0300

    update sdk version to 34 from 33

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.task2"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.task2"
         minSdk = 22
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
